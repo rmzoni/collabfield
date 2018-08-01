4e7aeb7b2e26e5e8070bcdcd391b4f9a94f6989c
Rafael Manzoni <rmzoni@gmail.com>
2018-07-31 23:41:50 -0300

    Add a spec for the Post model's default_scope

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Post, type: :model do
-  context 'Associations' do
-    it 'belongs_to user' do
+  context "Associations" do
+    it "belongs_to user" do
       association = described_class.reflect_on_association(:user).macro
       expect(association).to eq :belongs_to
     end
 
-    it 'belongs_to category' do
+    it "belongs_to category" do
       association = described_class.reflect_on_association(:category).macro
       expect(association).to eq :belongs_to
+    end
+  end
+
+  context "Scopes" do
+    it "default_scope orders by descending created_at" do
+      first_post = create(:post)
+      second_post = create(:post)
+      expect(Post.all).to eq [second_post, first_post]
     end
   end
 end
