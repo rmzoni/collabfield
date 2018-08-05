d67a14156244c38400eb30cea693a9390b47bad5
Rafael Manzoni <rmzoni@gmail.com>
2018-08-04 22:41:12 -0300

    Add specs for the Post model's validations

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,8 @@
-FactoryGirl.define do 
-    factory :post do
-        sequence(:title) { |n| "title#{n}" }
-        sequence(:content) { |n| "content#{n}" }
-        user factory: :user
-        category factory: :category
-    end
+FactoryGirl.define do
+  factory :post do
+    sequence(:title) { |n| "title#{n}" }
+    sequence(:content) { |n| "content-description-detailed#{n}" }
+    user factory: :user
+    category factory: :category
+  end
 end
